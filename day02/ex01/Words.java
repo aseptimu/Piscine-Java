8f5ea7109d7e1814abe002d105cfd7b708ca4bce
aseptimu <aseptimu@student.21-school.ru>
2022-07-10 13:15:31 +0300

    ex01 bad dictionary out + wrong similarity precision.

--- day02/ex01/Words.java
+++ day02/ex01/Words.java
@@ -13,13 +13,16 @@ public class Words {
 		List<String> wordsList1;
 		List<String> wordsList2;
 		Set<String> dictionary;
+		double similarity;
 
 		try {
 			wordsList1 = retrieveWordsFromFile(args[0]);
 			wordsList2 = retrieveWordsFromFile(args[1]);
 			dictionary = retrieveDictionaryFromLists(wordsList1, wordsList2);
 			writeDictionaryToFile(dictionary);
-		} catch (Exception e) {
+			similarity = calculateSimilarity(wordsList1, wordsList2, dictionary);
+			printSimilarity(similarity);
+		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
 	}
@@ -53,5 +56,56 @@ public class Words {
 			outFile.write(words + " ");
 		}
 		outFile.close();
+	}
+
+	private static double calculateSimilarity(List<String> words1, List<String> words2, Set<String> dictionary) {
+		int[] entryWords1 = retrieveWordsEntry(dictionary, words1);
+		int[] entryWords2 = retrieveWordsEntry(dictionary, words2);
+		double numerator = calculateNumerator(entryWords1, entryWords2, dictionary.size());
+		double denominator = calculateDenominator(entryWords1, entryWords2, dictionary.size());
+
+		return numerator / denominator;
+	}
+
+	private static int[] retrieveWordsEntry(Set<String> dictionary, List<String> words) {
+		int count;
+		int i = 0;
+		int[] entryWords = new int[dictionary.size()];
+
+		for (String wordInDictionary : dictionary) {
+			count = 0;
+			for (String wordInList : words) {
+				if (wordInList.equals(wordInDictionary)) {
+					count++;
+				}
+			}
+			entryWords[i] = count;
+			i++;
+		}
+		return entryWords;
+	}
+
+	private static int calculateNumerator(int[] entryWords1, int[] entryWords2, int size) {
+		int returnNumerator = 0;
+
+		for (int i = 0; i < size; i++) {
+			returnNumerator += entryWords1[i] * entryWords2[i];
+		}
+		return returnNumerator;
+	}
+
+	private static double calculateDenominator(int[] entryWords1, int[] entryWords2, int size) {
+		int sqrt1 = 0;
+		int sqrt2 = 0;
+
+		for (int i = 0; i < size; i++) {
+			sqrt1 += entryWords1[i] * entryWords1[i];
+			sqrt2 += entryWords2[i] * entryWords2[i];
+		}
+		return Math.sqrt(sqrt1) * Math.sqrt(sqrt2);
+	}
+
+	private static void printSimilarity (double similarity) {
+		System.out.printf("Similarity = %.2f", similarity);
 	}
 }
