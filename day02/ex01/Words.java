23ed6835125f17c36c2e9297a7ea0a623cbe9deb
aseptimu <aseptimu@student.21-school.ru>
2022-07-10 10:05:56 +0300

    ex 01 read/write

--- day02/ex01/Words.java
+++ day02/ex01/Words.java
@@ -1,44 +1,57 @@
 package ex01;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Words {
-	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+
+		if (args.length < 2) {
+			System.err.println("Not enough arguments");
+			System.exit(1);
+		}
+		List<String> wordsList1;
+		List<String> wordsList2;
+		Set<String> dictionary;
+
 		try {
-			BufferedReader file1 = new BufferedReader(new FileReader(scanner.next()));
-			BufferedReader file2 = new BufferedReader(new FileReader(scanner.next()));
-			BufferedWriter dictionaryFile = new BufferedWriter(new FileWriter("dictionary.txt"));
-			List<String> fileDictionary1 = null;
-			List<String> fileDictionary2 = null;
-			Set<String> dictionary = null;
-			String line;
-			String[] arrayOfWords;
-
-			while ((line = file1.readLine()) != null) {
-				arrayOfWords = line.split(" ");
-				fileDictionary1.addAll(Arrays.asList(arrayOfWords));
-				dictionary.addAll(fileDictionary1);
-			}
-
-			while ((line = file2.readLine()) != null) {
-				arrayOfWords = line.split(" ");
-				fileDictionary2.addAll(Arrays.asList(arrayOfWords));
-				dictionary.addAll(fileDictionary2);
-			}
-
-			for (String words : dictionary) {
-				dictionaryFile.write(words + " ");
-			}
-			file1.close();
-			file2.close();
-			dictionaryFile.close();
+			wordsList1 = retrieveWordsFromFile(args[0]);
+			wordsList2 = retrieveWordsFromFile(args[1]);
+			dictionary = retrieveDictionaryFromLists(wordsList1, wordsList2);
+			writeDictionaryToFile(dictionary);
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
+	}
+
+	private static List<String> retrieveWordsFromFile(String fileName) throws IOException {
+		BufferedReader file = new BufferedReader(new FileReader(fileName));
+		List<String> wordsList = null;
+		String line;
+		String[] words;
+
+		while ((line = file.readLine()) != null) {
+			words = line.split(" ");
+			wordsList = Arrays.asList(words);
+		}
+		file.close();
+		return wordsList;
+	}
+
+	private static Set<String> retrieveDictionaryFromLists(List<String> wordsList1, List<String> wordsList2) {
+		TreeSet<String> dictionary= new TreeSet<>();
+		dictionary.addAll(wordsList1);
+		dictionary.addAll(wordsList2);
+
+		return dictionary;
+	}
+
+	private static void writeDictionaryToFile(Set<String> dictionarySet) throws IOException {
+		BufferedWriter outFile = new BufferedWriter(new FileWriter("dictionary.txt"));
+
+		for (String words : dictionarySet) {
+			outFile.write(words + " ");
+		}
+		outFile.close();
 	}
 }
