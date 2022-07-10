b9701d0aeca272cebc76814f0dda8e7d6bf4c46f
aseptimu <aseptimu@student.21-school.ru>
2022-07-10 21:44:53 +0300

    mv added

--- day02/ex02/Commands.java
+++ day02/ex02/Commands.java
@@ -1,24 +1,51 @@
 package ex02;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class Commands {
-	String currentPath;
+	private final Path currentPath;
 
-	public Commands(String currentPath) {
+	public Commands(Path currentPath) throws IOException {
+		System.out.println(currentPath);
+		if (Files.exists(currentPath)) {
+			throw new IOException("File not found"); // TODO: check
+		}
 		this.currentPath = currentPath;
 	}
 
 	public void mv(Path from, Path to) {
-//		Files.createFile();
+		try {
+			Path fromAbsolute = Paths.get(this.currentPath + "/" + from);
+			Path toAbsolute = Paths.get(this.currentPath + "/" + to);
+
+			if (Files.isRegularFile(fromAbsolute)) {
+				if (Files.isDirectory(toAbsolute)) {
+					toAbsolute = Paths.get(toAbsolute + "/" + fromAbsolute.getFileName());
+				Files.move(fromAbsolute, toAbsolute, REPLACE_EXISTING);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 	public void ls() {
+		List<Path> files;
+
 
 	}
 
 	public void cd() {
 
+	}
+
+	public Path getCurrentPath() {
+		return currentPath;
 	}
 }
--- day02/ex02/FileManager.java
+++ day02/ex02/FileManager.java
@@ -8,7 +8,7 @@ public class FileManager {
 	private static final String START_FLAG = "--current-folder=";
 
 	public static void main(String[] args) {
-		Path currentPath = null;
+		Path currentPath;
 
 		if (args.length == 1 && args[0].startsWith(START_FLAG)) {
 			currentPath = Paths.get(args[0].substring(START_FLAG.length()));
@@ -18,17 +18,20 @@ public class FileManager {
 		}
 
 		try {
+			Commands commands = new Commands(currentPath);
 			Scanner scanner = new Scanner(System.in);
 			String line;
 			String[] cmd;
 			while (!(line = scanner.nextLine()).equals("exit")) {
 				cmd = line.split(" ");
 				switch (cmd[0]) {
+					case "mv":
+						commands.mv(Paths.get(cmd[1]), Paths.get(cmd[2]));
+						break;
 					case "ls":
+						System.out.println("ls"); //TODO: delete
 						break;
 					case "cd":
-						break;
-					case "mv":
 						break;
 					default:
 						System.out.println("Wrong cmd");
