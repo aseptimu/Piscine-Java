149206acd1996716b7c6ee09852a766ef87cc542
aseptimu <aseptimu@student.21-school.ru>
2022-07-17 14:13:24 +0300

    ex00 begin

---
+++ day05/ex00/Chat/src/main/java/edu/school21/chat/Message.java
@@ -0,0 +1,91 @@
+package Chat.src.main.java.edu.school21.chat;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Message {
+	private Integer id;
+	private String author;
+	private Chatroom room;
+	private String text;
+	private LocalDateTime messageDateTime;
+
+	public Message(Integer id, String author, Chatroom room, String text, LocalDateTime messageDateTime) {
+		this.id = id;
+		this.author = author;
+		this.room = room;
+		this.text = text;
+		this.messageDateTime = messageDateTime;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id;
+		result = 31 * result + id.hashCode();
+		result = 31 * result + author.hashCode();
+		result = 31 * result + room.hashCode();
+		result = 31 * result + text.hashCode();
+		result = 31 * result + messageDateTime.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Message msg)) {
+			return false;
+		}
+		return id.equals(msg.id);
+	}
+
+	@Override
+	public String toString() {
+		return ("Message id: " + id +
+				" author: " + author +
+				" room: " + room +
+				" content: " + text +
+				" date: " + messageDateTime);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Chatroom getRoom() {
+		return room;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public LocalDateTime getMessageDateTime() {
+		return messageDateTime;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setRoom(Chatroom room) {
+		this.room = room;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setMessageDateTime(LocalDateTime messageDateTime) {
+		this.messageDateTime = messageDateTime;
+	}
+}
