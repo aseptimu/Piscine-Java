8ad93f9b5a87d922e57423dca6d867c4d13ae38e
aseptimu <aseptimu@student.21-school.ru>
2022-07-24 18:23:28 +0300

    ex01 begin  + some bugs fixed

---
+++ day06/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, price) VALUES ('Milk', 100);
+INSERT INTO product (name, price) VALUES ('Egg', 120);
+INSERT INTO product (name, price) VALUES ('Cake', 150);
+INSERT INTO product (name, price) VALUES ('Chocolate', 140);
+INSERT INTO product (name, price) VALUES ('Bread', 40);
